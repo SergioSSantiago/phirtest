529182b1e7e95cec88f60ac0eef9d46167c98d36
Sergio Sapiña Santiago <sergiosapinasantiago@MacBook-Air-de-Sergio.local>
2020-08-31 14:23:26 +0200

    medeotest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Practitioner: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://hapi.fhir.org/baseR4/Practitioner')
+      .then((response) => {
+        return response.json()
+      })
+      .then((pract) => {
+        this.setState({ Practitioner: pract.entry })
+      })    
+  }
+
+  render() {
+    return (
+      <div>
+        <table border="1">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Family name</th>
+            <th>BirthDate</th>                    
+          </tr>
+        </thead>
+        <tbody>  
+          {this.state.Practitioner.map(pract => {
+            return (
+              <tr key={pract.fullUrl}>
+                <td>{pract.resource.id}</td>
+                <td>{pract.resource.name ? pract.resource.name[0].family : ''}</td>
+                <td>{pract.resource.birthDate}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        </table>
+      </div>
+    );
+  }
+   
 }
 
 export default App;
